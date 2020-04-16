62612fb9f1046bd4aa37f3ce032456986b8ab78e
Lucas <lucasatletico@hotmail.com>
2020-04-16 14:01:59 -0300

    Criando projeto e base de teste JUnit

---
+++ teste/br/com/coder/cm/Teste.java
@@ -0,0 +1,16 @@
+package br.com.coder.cm;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Teste {
+
+	@Test
+	void testarIgual() {
+		int a = 1 +1;
+		assertEquals(2, a);
+		
+	}
+
+}
